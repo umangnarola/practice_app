c549a776f196f238c54069e3652b3b69af508f58
shantilal-narola <shm.narola@gmail.com>
2017-08-29 17:41:37 +0530

    modified file after removing validation

--- app/models/prodact.rb
+++ app/models/prodact.rb
@@ -1,11 +1,9 @@
 class Prodact < ApplicationRecord
-  validates_presence_of :name,:decription,:price,:quantity
+  validates_presence_of :name,:price
   has_many :order_items
   scope :prodacts, -> {
   where(:is_active =>  true)
 }
-  # default_scope { where(active: true) }
   belongs_to :user ,optional: true
   resourcify
-
 end

e54cdaaa050a8d967bb44ffffbaae8793d1a8ad6
shantilal-narola <shm.narola@gmail.com>
2017-08-29 11:59:20 +0530

    modify file after change production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.default_url_options = { :host => 'locahost:3000' }
+  config.action_mailer.default_url_options = { :host => 'limitless-woodland-66775.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
